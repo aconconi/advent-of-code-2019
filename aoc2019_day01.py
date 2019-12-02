ea6c655dfa2d94b73c3a531b41a3fa0b2a4e560f
aconconi <alex.conconi@frogdesign.com>
2019-12-02 15:21:28 +0100

    2019 day 2 solution

--- aoc2019_day01.py
+++ aoc2019_day01.py
@@ -3,14 +3,9 @@
 
 from math import floor
 
-# read input file into lines
+# read input file and load data into array of integers
 with open("data/day01.dat", "r") as data_file:
-    lines = data_file.readlines()
-
-# load data into array of integers
-data = []
-for line in lines:
-    data.append(int(line))
+        data = [int(line.strip()) for line in data_file]
 
 # to find the fuel required for a module,
 # take its mass, divide by three, round down, and subtract 2.
@@ -31,11 +26,21 @@ def day01part1(data):
 def day01part2(data):
     return sum(secondary_fuel(m) for m in data)
 
+# Test cases
+assert fuel(12) == 2
+assert fuel(14) == 2
+assert fuel(1969) == 654
+assert fuel(100756) == 33583
+assert secondary_fuel(14) == 2
+assert secondary_fuel(1969) == 966
+assert secondary_fuel(100756) == 50346
 
-print("What is the sum of the fuel requirements for all of the modules\
+# Part 1
+print("What is the sum of the fuel requirements for all of the modules on\
 on your spacecraft?")
 print(day01part1(data)) # Correct answer is 3394689
 
+# Part 2
 print("What is the sum of the fuel requirements for all of the modules on\
 your spacecraft when also taking into account the mass of the added fuel? ")
 print(day01part2(data)) # Correct answer is 5089160
