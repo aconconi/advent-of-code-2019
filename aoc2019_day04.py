e72499990a7645c7ed51f850e0c6aededbb26f85
aconconi <alex.conconi@frogdesign.com>
2019-12-04 22:00:33 +0000

    tidy up

--- aoc2019_day04.py
+++ aoc2019_day04.py
@@ -1,21 +1,24 @@
 # Advent of Code 2019
 # Day 4: Secure Container
 
-def get_digit(n, i):
-    return n // 10**i % 10
-
-# initially implemented this to generate list of digits from number
-# however generating a string instead works just fine
+# generate indexable digits from number
+# using strings makes the rest of the program faster
 def number_to_digits(n):
     return str(n)
-    # return [ get_digit(n, i) for i in range(len(str(n))) ]
+
+# alternative implementation 2
+# using lists makes the rest of the program faster
+def number_to_digits2(n):
+    def get_digit(n, i):
+        return n // 10**i % 10
+    return [ get_digit(n, i) for i in reversed(range(len(str(n)))) ]
+
 
 # At least two adjacent digits are the same
 def same_adjacent_digits(n):
     digits = number_to_digits(n)  
     return True if [d for i, d in enumerate(digits[:-1]) if digits[i] == digits[i+1] ] else False
 
-# At least two adjacent digits are the same
 # alternative implementation 2, same speed
 def same_adjacent_digits2(n):
     digits = number_to_digits(n)
@@ -24,12 +27,12 @@ def same_adjacent_digits2(n):
             return True
     return False
 
-# At least two adjacent digits are the same
 # alternative implementation 3, same speed
 def same_adjacent_digits3(n):
     digits = number_to_digits(n)
     d1,d2,d3,d4,d5,d6 = digits[0], digits[1], digits[2], digits[3], digits[4], digits[5]
     return d1 == d2 or d2 == d3 or d3 == d4 or d4 == d5 or d5 == d6
+
 
 # check that going from left to right the digits never decrease
 def digits_never_decrease(n):
@@ -39,7 +42,6 @@ def digits_never_decrease(n):
             return False
     return True
 
-# check that going from left to right the digits never decrease
 # alternative implementation 2, functional syle, slower
 def digits_never_decrease2(n):
     digits = number_to_digits(n)  
@@ -83,8 +85,8 @@ def day04part2(low, high):
 
 # Part 1
 print("How many different passwords within the range given in your puzzle input meet these criteria?")
-print(day04part1(171309,643603)) # correct answer is 1625
+print(day04part1(171309, 643603)) # Correct answer is 1625
 
 # Part 2
 print("How many different passwords within the range given in your puzzle input meet all of the criteria?")
-print(day04part2(171309,643603)) # correct answer is 1111
+print(day04part2(171309, 643603)) # Correct answer is 1111
