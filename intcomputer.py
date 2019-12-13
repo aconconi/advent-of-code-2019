9b57ca606323b3a97417b0be191f7e9b35ffaba1
aconconi <alex.conconi@frogdesign.com>
2019-12-13 16:06:18 +0100

    refactoring

--- intcomputer.py
+++ intcomputer.py
@@ -175,36 +175,36 @@ class TestIntComputer(unittest.TestCase):
         for i in [7, 8, 9]:
             computer = IntComputer(program, [i])
             computer.run()
-            self.assertTrue(  computer.pop_output() == (i == 8) )
+            self.assertEqual(computer.pop_output(), (i == 8))
 
         program = [3, 9, 7, 9, 10, 9, 4, 9, 99, -1, 8]
         for i in [7, 8, 9]:
             computer = IntComputer(program, [i])
             computer.run()        
-            self.assertTrue(  computer.pop_output() == (i < 8) )
+            self.assertEqual(computer.pop_output(), (i < 8))
 
         program = [3, 3, 1108, -1, 8, 3, 4, 3, 99]
         for i in [7, 8, 9]:
             computer = IntComputer(program, [i])
             computer.run()
-            self.assertTrue(  computer.pop_output() == (i == 8) )
+            self.assertEqual(computer.pop_output(), (i == 8))
 
         program = [3, 3, 1107, -1, 8, 3, 4, 3, 99]
         for i in [7, 8, 9]:
             computer = IntComputer(program, [i])
             computer.run()
-            self.assertTrue(  (computer.pop_output() == (i < 8)) )
+            self.assertEqual(computer.pop_output(), (i < 8))
 
         for i in [0, 1, -2]:
             program = [3, 12, 6, 12, 15, 1, 13, 14, 13, 4, 13, 99, -1, 0, 1, 9]
             computer = IntComputer(program, [i])
             computer.run()
-            self.assertTrue(  (computer.pop_output() == (i != 0)) )
+            self.assertEqual(computer.pop_output(), (i != 0))
             
             program = [3, 3, 1105, -1, 9, 1101, 0, 0, 12, 4, 12, 99, 1]
             computer = IntComputer(program, [i])
             computer.run()
-            self.assertTrue(  (computer.pop_output() == (i != 0)) )
+            self.assertEqual(computer.pop_output(),  (i != 0))
 
         program = [3, 21, 1008, 21, 8, 20, 1005, 20, 22, 107, 8, 21, 20, 1006, 20, 31, 1106, 0, 36, 98, 0, 0, 1002, 21, 125,
                 20, 4, 20, 1105, 1, 46, 104, 999, 1105, 1, 46, 1101, 1000, 1, 20, 4, 20, 1105, 1, 46, 98, 99]
