91f3ee8c5e7ce277065b004555eb9c7500b47be7
aconconi <alex.conconi@frogdesign.com>
2019-12-16 19:21:41 +0100

    2019 day 16 part 2 solution

--- aoc2019_day16.py
+++ aoc2019_day16.py
@@ -25,16 +25,18 @@ def rotate_left(a):
 
 def expand(pattern, n, max_len):
     new = list(chain.from_iterable(zip(*repeat(pattern, n))))
-    return new
+    return new[:max_len+1]
 
 def apply(pattern, signal):
     # print(f"Pattern: {pattern}, signal: {signal}")
     return abs(sum( s * p for s, p in zip(signal, cycle(rotate_left(pattern))))) % 10
 
+
 def phase(signal):
     r = []
     for i in range(len(signal)):
         pattern = expand(BASE_PATTERN, i+1, len(signal))
+        # pattern = deque([p for p in [0, 1, 0, -1] for _ in range(i+1)])
         r.append(apply(pattern, signal))
     return r
 
@@ -58,42 +60,44 @@ def day16part1(signal_str, phases):
     #         new = new[:max_len]
     #         break
 
-def day16part2(signal_str, phases):
-    length = len(signal_str)
+def mul(k, n):
+    return BASE_PATTERN[ (k % (n*4)) // n ]
+
+def day16part2(signal_str):
     offset = int(signal_str[:7])
-    times  = 10000
     # print(f"Offset: {offset}")
-    signal = [int(c) for c in signal_str] * times
-    # print(f"Starting phases with signal {signal}, len {length}")
-    for i in range(phases):
-        # print(f"Phase {i}...")
-        signal = phase(signal)
-        
-    result = list_to_string(signal[offset:offset+8])
-    return result
+    signal = [int(c) for c in signal_str] * 10000
+    signal = signal[offset:]
+    for f in range(100):
+        # print(f"Starting phase {f+1} with signal len {len(signal)}")
+        for i in range(-2, -len(signal)-1, -1):
+            signal[i] = (signal[i] + signal[i+1]) % 10
+
+    return "".join([str(x) for x in signal[:8]])
+
     
 
 
 # Test cases for part 1
-test_data1 = '12345678' # becomes 01029498 after 4 phases 
-test_data2 = '80871224585914546619083218645595' # becomes 24176176 after 100 phases
-test_data3 = '19617804207202209144916044189917' # becomes 73745418 after 100 phases
-test_data4 = '69317163492948606335995924319873' # becomes 52432133 after 100 phases
-assert day16part1(test_data1, 4) == '01029498'
-assert day16part1(test_data2, 100) == '24176176'
-assert day16part1(test_data3, 100) == '73745418'
-assert day16part1(test_data4, 100) == '52432133'
+assert day16part1('12345678', 4) == '01029498'
+assert day16part1('80871224585914546619083218645595', 100) == '24176176'
+assert day16part1('19617804207202209144916044189917', 100) == '73745418'
+assert day16part1('69317163492948606335995924319873', 100) == '52432133'
 
 with open("data/day16.dat", "r") as data_file:
         data = data_file.readline().strip()
 
 # Part 1
-# print("After 100 phases of FFT, what are the first eight digits in the final output list?")
-# print(day16part1(data, 100))
+print("After 100 phases of FFT, what are the first eight digits in the final output list?")
+print(day16part1(data, 100)) # Correct answer is 73127523
+
+
+# Test cases for part 2
+assert day16part2('03036732577212944063491565474664') == '84462026'
+assert day16part2('02935109699940807407585447034323') == '78725270'
+assert day16part2('03081770884921959731165446850517') == '53553731'
 
 # Part 2
-# print("Part 2")
-# test_data5 = '03036732577212944063491565474664'
-# print(day16part2(test_data5, 1))  # Correct answer is 73127523
-
-# print(expand(BASE_PATTERN, 8))
+print("What is the eight-digit message embedded in the final output list?")
+test_data5 = '03036732577212944063491565474664'
+print(day16part2(data))  # Correct answer is ?
